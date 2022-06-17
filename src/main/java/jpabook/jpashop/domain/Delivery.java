548205be4fc0245e3c8c5ae23cfbcfea6e0bdac3
KyungNam Ko <rhrudska987@naver.com>
2022-06-17 19:24:50 +0900

    2022.06.17 Study 야생형 첫번째 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 도메인 구현과 분석

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.*;
+
+@Entity
+@Getter@Setter
+public class Delivery {
+
+    @Id@GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+//OneToOne도 fetch를 LAZY로 해줘야 함
+    @OneToOne(mappedBy = "delivery", fetch = LAZY)
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) //무조건 스트링으로
+    private DeleiveryStatus status; //READY, COMP
+}
